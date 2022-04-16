9885a2b9b4751f92242929059b55680beb415a20
Francisco Sicardi <fsicardir@gmail.com>
2022-04-16 11:50:21 -0300

    format code

--- src/main/App.js
+++ src/main/App.js
@@ -12,47 +12,47 @@ const swaggerDoc = swaggerJsDoc(swaggerConfig);
 const routes = require('./routes')
 
 class App {
-    constructor() {
-        this.app = express();
-        this.app.use(cors());
-        this.app.use(bodyParser.json());
+  constructor() {
+    this.app = express();
+    this.app.use(cors());
+    this.app.use(bodyParser.json());
+  }
+
+  async syncDB() {
+    if (!constants.isDevelopment) {
+      await runMigrations();
     }
 
-    async syncDB() {
-        if (!constants.isDevelopment) {
-            await runMigrations();
-        }
+    // "sync()" creates the database table for our model(s),
+    // if we make .sync({force: true}),
+    // the db is dropped first if it is already existed
+    await database.sync({
+      force: constants.RESET_DATABASE
+    });
 
-        // "sync()" creates the database table for our model(s),
-        // if we make .sync({force: true}),
-        // the db is dropped first if it is already existed
-        await database.sync({
-            force: constants.RESET_DATABASE
-        });
+    this.app
+      .listen(constants.NODE_PORT, () => {
+        console.log(`Listening on port ${constants.NODE_PORT}`);
+      });
+  }
 
-        this.app
-            .listen(constants.NODE_PORT, () => {
-                console.log(`Listening on port ${constants.NODE_PORT}`);
-            });
-    }
+  defineLogLevel() {
+    Logger.setLevel(constants.LOG_LEVEL);
+  }
 
-    defineLogLevel() {
-        Logger.setLevel(constants.LOG_LEVEL);
-    }
-
-    defineEvents() {
-        this.app.use('/', routes);
-        this.app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
-    }
+  defineEvents() {
+    this.app.use('/', routes);
+    this.app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+  }
 }
 
 const main = new App();
 
 main.syncDB()
-    .then(() => {
-        main.defineLogLevel();
-        main.defineEvents();
-    })
-    .catch((error) => {
-        console.log(error);
-    });
+  .then(() => {
+    main.defineLogLevel();
+    main.defineEvents();
+  })
+  .catch((error) => {
+    console.log(error);
+  });
