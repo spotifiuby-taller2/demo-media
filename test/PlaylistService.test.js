25e6e4cffe516dccc0d0069429cf8ebb226faf66
Francisco Sicardi <fsicardir@gmail.com>
2022-04-16 16:07:49 -0300

    add test

--- test/PlaylistService.test.js
+++ test/PlaylistService.test.js
@@ -17,8 +17,8 @@ describe('PlaylistService', function() {
 
       await PlaylistService.getPlaylist(req, res);
 
-      assert(res.status.calledWith(200))
-      assert(res.json.calledWith(playlist))
+      assert(res.status.calledWith(200));
+      assert(res.json.calledWith(playlist));
       revertRewire();
     });
 
@@ -32,15 +32,15 @@ describe('PlaylistService', function() {
 
       await PlaylistService.getPlaylist(req, res);
 
-      assert(res.status.calledWith(404))
-      assert(res.json.called)
+      assert(res.status.calledWith(404));
+      assert(res.json.called);
       revertRewire();
     });
   });
   describe('getPlaylists', function () {
     it('returns a filtered list of playlists', async function () {
       const playlist = {};
-      let playlists = [playlist, playlist, playlist];
+      const playlists = [playlist, playlist, playlist];
       const playlistFindAll = sinon.fake.returns(Promise.resolve(playlists))
       const revertRewire = PlaylistService.__set__({
         Playlist: {findAll: playlistFindAll},
@@ -51,10 +51,49 @@ describe('PlaylistService', function() {
 
       await PlaylistService.getPlaylists(req, res);
 
-      assert(res.status.calledWith(200))
-      assert(res.json.calledWith(playlists))
-      assert(playlistFindAll.calledWith(sinon.match({where: {owner: ownerId}})))
+      assert(res.status.calledWith(200));
+      assert(res.json.calledWith(playlists));
+      assert(playlistFindAll.calledWith(sinon.match({where: {owner: ownerId}})));
       revertRewire();
     })
   });
+  describe('newPlaylist', function () {
+    it('creates a new playlist', async function() {
+      const songIds = [1, 2, 3];
+      const songs = [{}, {}, {}]
+      const songFindAllFake = sinon.fake.returns(Promise.resolve(songs))
+      const playlistAddSongsFake = sinon.fake.returns(Promise.resolve());
+      const playlist = {
+        addSongs: playlistAddSongsFake,
+        title: 'some title',
+        owner: 'the owner',
+      };
+      playlist.get = () => playlist;
+      const playlistCreateFake = sinon.fake.returns(Promise.resolve(playlist));
+      const revertRewire = PlaylistService.__set__({
+        Song: {findAll: songFindAllFake},
+        Playlist: {create: playlistCreateFake},
+      });
+      const req = {body: {
+        title: 'some title',
+        owner: 'the owner',
+        songs: songIds,
+      }};
+      const res = mockResponse();
+
+      await PlaylistService.newPlaylist(req, res);
+
+      assert(res.status.calledWith(200));
+      assert(res.json.calledWith(sinon.match({
+        title: 'some title',
+        owner: 'the owner',
+        songs: songs,
+      })));
+      assert(songFindAllFake.calledWith(sinon.match({where: {id: songIds}})));
+      assert(playlistCreateFake.calledWith(sinon.match.has('title', 'some title')
+        .and(sinon.match.has('owner', 'the owner'))));
+      assert(playlistAddSongsFake.calledWith(songs));
+      revertRewire();
+    })
+  })
 });
