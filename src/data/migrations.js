5e52bf0ec14dfa4ea315de72c1382bb3e229736f
Esteban <edjeordjian@fi.uba.ar>
2022-07-03 22:45:47 -0300

    Fix proposal

--- src/data/migrations.js
+++ src/data/migrations.js
@@ -8,29 +8,6 @@ const {Song,
 const queryInterface = database.getQueryInterface();
 
 async function runMigrations() {
-    /* ================ EXAMPLES ===================== */
-    /* await queryInterface.removeColumn('users',
-                                  'isAdmin');
-                       .catch(e => {
-                        console.log(e);
-                       } ); */
-    /*
-       await queryInterface.addColumn(Song.tableName,
-           'isBlocked', {
-                  type: Sequelize.BOOLEAN,
-                  allowNull: false,
-                  defaultValue: false
-           }).catch(error => console.log(error.toString())); */
-
-
-    /* await queryInterface.changeColumn('users ',
-      'walletId', {
-        type: Sequelize.INTEGER,
-        unique: true
-      },)
-      .catch(error => {
-        console.log(error.toString());
-      });*/
 }
 
 module.exports = {
